e972d0e6567488133284936f0165498f3488d8a3
AvinashMarkad <markadavi3719@gmail.com>
2025-03-19 12:29:38 +0530

    string functions

---
+++ string1.py
@@ -0,0 +1,45 @@
+# str = "name123"
+# str1 = "name123"
+# str2 = "name123"
+
+# list = []
+# list1 = []
+# list2 = []
+
+# for i in str:
+#     if i.isnumeric():
+#         list.append(i)
+    
+# print(list) 
+
+# for i in str1:
+   
+#     if i.isalpha():
+#         list1.append(i)
+   
+# print(list1) 
+
+# for i in str2:
+   
+#     if i.isalnum():
+#         list2.append(i)
+# print(list2) 
+
+# ........................................................................................................
+
+# x = "coding cafe"
+# y=""
+
+# for i in x:
+#     if i.isalpha():
+#         y +=i
+# print(y) 
+
+# ........................................................................................................
+
+x = str(input("Enter a string: "))
+
+if x.isalpha():
+    print("The string is alphabet")
+else:
+    print("The string is not alphabet")
