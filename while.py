f0a6efb2b6af1a6b9fd1451b0ab1491628b3f444
AvinashMarkad <markadavi3719@gmail.com>
2025-03-19 11:26:32 +0530

    range

--- while.py
+++ while.py
@@ -1,5 +1,46 @@
-a = 2 
-while(a < 20):
-    print(a)
-    a += 2
-print("code reached end of while loop")
+# a = 2 
+# while(a < 20):
+#     print(a)
+#     a += 2
+# else:
+#     print("code reached when while condition is false")   
+# print("code reached end of while loop")
+
+# .............................................................................................
+
+# i=0
+
+# while True:
+#     i+=1
+#     print(i)
+#     if i == 5:
+#         break
+# print("code reached end of while loop")
+
+
+# ......................................................................................................
+
+# i=0
+
+# while True:
+#     i+=1
+#     if i == 5:
+#         break
+#     print(i)
+   
+# print("code reached end of while loop")
+
+# ........................................................................................................
+
+i =1
+
+while i<=3:
+    print("outer loop",i)
+    i+=1
+    j=1
+    while (j<=5):
+        print("inner loop",j)
+        j+=1
+print("end of the code ")    
+
+
