4e3cb9b25ab459cd95560c5131b74868e7625a44
AvinashMarkad <markadavi3719@gmail.com>
2025-03-20 10:13:53 +0530

    list append

---
+++ listAppend.py
@@ -0,0 +1,9 @@
+a = []
+
+n = int(input("Enter the number of elements in the list: "))
+
+for i in range(n):
+    a.append(int(input("Enter the element: ")))
+
+print("The list is: ",a)
+print("The length of the list is: ",len(a))
