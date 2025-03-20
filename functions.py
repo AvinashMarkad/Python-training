f9ba578bf55415a2ca675309010f750472ed7c70
AvinashMarkad <markadavi3719@gmail.com>
2025-03-20 14:47:41 +0530

    Variable Length Args, local & global variable

--- functions.py
+++ functions.py
@@ -20,13 +20,32 @@
 
 #.............................................................................
 
-def display(st):
-    def show():
-        return "show function "
-    result = show() + st + " display function"
-    return result
+# def display(st):
+#     def show():
+#         return "show function "
+#     result = show() + st + " display function"
+#     return result
 
-print(display("Hello"))
+# print(display("Hello"))
 
 #.............................................................................
 
+# def positionalArgs(a,b,c):
+#     print(a,b,c)
+
+# positionalArgs(10,20,30)
+
+#.............................................................................
+
+# def keywordArgs(a,b,c):
+#     print(a,b,c)
+
+# keywordArgs(c=30,a=10,b=20)
+
+#.............................................................................
+
+def defaultArgs(a,b,c=30):
+    print(a,b,c)
+
+defaultArgs(10,20)
+defaultArgs(10,20,40)
